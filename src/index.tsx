3bbedbfcc362158864e60d6666be66ba2fec3c21
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-11-14 11:06:31 +0300

    fix: move deploy to subdomain

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/test__vipservice-avia">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
