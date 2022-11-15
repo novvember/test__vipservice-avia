d8fa519a55dee46fe9ff2ac7c73e2fc5f0ca1fd6
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-11-15 12:21:31 +0300

    feat: add layout for flights

--- src/components/Flight/Flight.tsx
+++ src/components/Flight/Flight.tsx
@@ -1,50 +1,71 @@
 import './Flight.css';
-import logo from '../../images/logo_s7.png';
-import luggageIcon from '../../images/icon__luggage_active.svg';
-import cabinLuggageIcon from '../../images/icon__cabin-luggage_active.svg';
 import { Flight as FlightType } from '../../types/tickets';
+import logo from '../../images/logo_s7.png';
+import luggageActiveIcon from '../../images/icon__luggage_active.svg';
+import cabinLuggageActiveIcon from '../../images/icon__cabin-luggage_active.svg';
+import luggageIcon from '../../images/icon__luggage.svg';
+import cabinLuggageIcon from '../../images/icon__cabin-luggage.svg';
 
 function Flight({ flight }: { flight: FlightType }) {
   return (
-    <div className="ticket__ticket">
-      <div className="ticket__airline">
+    <li className="flight">
+      {!flight.refundable && (
+        <span className="flight__special">Невозвратный</span>
+      )}
+
+      <div className="flight__carrier">
         <img
-          className="ticket__airline-logo"
+          className="flight__carrier-logo"
           src={logo}
           alt="Логотоип авиакомпании"
         />
-        <p className="ticket__airline-title">{'ticket.carrier'}</p>
+        <p className="flight__carrier-title">{flight.carrier}</p>
       </div>
 
-      <div className="ticket__info">
-        <div className="ticket__track">
-          <div className="ticket__point">
-            <span className="ticket__time">9:20</span>
-            <span className="ticket__city">Москва</span>
-            <span className="ticket__date">14.11.2022</span>
+      <div className="flight__info">
+        <div className="flight__way">
+          <div className="flight__station flight__station_type_A">
+            <span className="flight__time">9:20</span>
+            <span className="flight__city">Москва</span>
+            <span className="flight__date">14.11.2022</span>
           </div>
-          <div className="ticket__way">
-            <span className="ticket__code">SVO</span>
-            <span className="ticket__duration">В пути 1 ч 55 мин</span>
-            <span className="ticket__code">SVO</span>
+          <div className="flight__track">
+            <span className="flight__code">SVO</span>
+            <span className="flight__duration">В пути 1 ч 55 мин</span>
+            <span className="flight__code">SVO</span>
           </div>
-          <div className="ticket__point">
-            <span className="ticket__time">9:20</span>
-            <span className="ticket__city">Москва</span>
-            <span className="ticket__date">14.11.2022</span>
+          <div className="flight__station flight__station_type_B">
+            <span className="flight__time">9:20</span>
+            <span className="flight__city">Москва</span>
+            <span className="flight__date">14.11.2022</span>
           </div>{' '}
         </div>
-        <div className="ticket__switch">1 2 3</div>
+        <div className="flight__tabs">Tabs will be here</div>
       </div>
-      <div className="ticket__luggage">
+
+      <div className="flight__luggage">
         <img
-          src={cabinLuggageIcon}
+          src={flight.cabbinLuggage ? cabinLuggageActiveIcon : cabinLuggageIcon}
           alt="Ручная кладь"
-          className="ticket__luggage-icon"
+          title={
+            flight.cabbinLuggage
+              ? 'Ручная кладь входит в стоимость'
+              : 'Ручная кладь оплачивается отдельно'
+          }
+          className="flight__luggage-icon"
         />
-        <img src={luggageIcon} alt="Багаж" className="ticket__luggage-icon" />
+        <img
+          src={flight.luggage ? luggageActiveIcon : luggageIcon}
+          alt="Багаж"
+          title={
+            flight.luggage
+              ? 'Багаж входит в стоимость'
+              : 'Багаж оплачивается отдельно'
+          }
+          className="flight__luggage-icon"
+        />
       </div>
-    </div>
+    </li>
   );
 }
 
